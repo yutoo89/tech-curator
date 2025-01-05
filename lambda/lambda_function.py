51428a3977589492242047e140f5c19dbd474e8e
yutoo89 <yuta.o.note@gmail.com>
2025-01-05 15:30:57 +0900

    end session after set topic

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -127,7 +127,7 @@ class SetTopicIntentHandler(AbstractRequestHandler):
 
         return (
             handler_input.response_builder.speak(speak_output)
-            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .set_should_end_session(True)
             .response
         )
 
